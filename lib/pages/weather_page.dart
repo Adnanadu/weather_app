4185cc476f80daf26984332a334e6409852e102a
AdnanAdu <adnanpe5@gmail.com>
2025-02-07 15:53:59 +0530

    final stage

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,10 +1,110 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class WeatherPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weather_app/core/secret/env/env.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
 
   @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  ///apikey
+  final weatherService = WeatherService(Env.apiKey);
+  WeatherModel? weatherModel;
+
+  ///fetch weather
+  void fetchWeather() async {
+    ///get current city
+    String cityName = await weatherService.getCurrentCity();
+
+    ///get weather city
+    try {
+      WeatherModel weather = await weatherService.getWeather(cityName);
+      setState(() {
+        weatherModel = weather;
+      });
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  ///weather animation
+  String getWeatherAnimations(String? mainCondition) {
+    switch (mainCondition) {
+      case 'Clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/animations/cloud.json';
+      case 'Rain':
+      case 'drizzle':
+        return 'assets/animations/rain.json';
+      case 'shower rain':
+      case 'thunderstorm':
+        return 'assets/animations/rain_with_thunder.json';
+      case 'Clear':
+        return 'assets/animations/sunny.json';
+      case '':
+        return 'assets/animations/sunny.json';
+      default:
+        return 'assets/animations/cloud.json';
+    }
+  }
+
+  ///init state
+  @override
+  void initState() {
+    fetchWeather();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //city name
+            Column(
+              spacing: 10,
+              children: [
+                Icon(
+                  Icons.location_on,
+                  color: Colors.grey,
+                  size: 30,
+                ),
+                Text(weatherModel?.cityName ?? 'Loading City...!',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey)),
+              ],
+            ),
+
+            //animations
+            Lottie.asset(getWeatherAnimations(weatherModel?.mainContinion)),
+
+            ///temperature
+            Text(
+              "${weatherModel?.temperature} °C",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
 
 import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
-  static const Base_URL = "https://api.openweathermap.org/data/2.5/weather";
+  static const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
   final String apiKey;
   WeatherService(this.apiKey);
   Future<WeatherModel> getWeather(String cityName) async {
     final response = await http
-        .get(Uri.parse('$Base_URL?q=$cityName&appid=$apiKey&units=metric'));
+        .get(Uri.parse('$baseUrl?q=$cityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return WeatherModel.fromJson(jsonDecode(response.body));
@@ -19,7 +20,7 @@ class WeatherService {
     }
   }
 
-  Future getCurrentCity() async {
+  Future<String> getCurrentCity() async {
     bool serviceEnabled;
     LocationPermission permission;
 
@@ -42,5 +43,20 @@ class WeatherService {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
+
+    /// Get the current position
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+      ),
+    );
+
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+    String? city = placemarks[0].locality;
+    return city ?? "";
   }
 }
